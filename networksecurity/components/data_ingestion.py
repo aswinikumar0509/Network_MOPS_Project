625eef37ab9c2204a7c73ad253f72596604a1074
Aswini Kumar <aswini0509@gmail.com>
2026-03-03 23:51:24 +0530

    update library

---
+++ networksecurity/components/data_ingestion.py
@@ -0,0 +1,17 @@
+from networksecurity.exception.exception import NetworkSecurityException
+from networksecurity.logging.logger import logging
+
+
+## configuration of the Data Ingestion Config
+
+from networksecurity.entity.config_entity import DataIngestionConfig
+from networksecurity.entity.artifact_entity import DataIngestionArtifact
+import os
+import sys
+import numpy as np
+import pandas as pd
+import pymongo
+from typing import List
+from sklearn.model_selection import train_test_split
+from dotenv import load_dotenv
+load_dotenv()
