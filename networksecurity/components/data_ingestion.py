789d981e6bf42f11c8040a89d2bdc3e5ca3754af
Aswini Kumar <aswini0509@gmail.com>
2026-03-06 23:24:00 +0530

    dataingestion 1

--- networksecurity/components/data_ingestion.py
+++ networksecurity/components/data_ingestion.py
@@ -40,3 +40,44 @@ class DataIngestion:
         except Exception as e:
             raise NetworkSecurityException
         
+    def export_data_into_feature_store(self,dataframe: pd.DataFrame):
+        try:
+            feature_store_file_path=self.data_ingestion_config.feature_store_file_path
+            #creating folder
+            dir_path = os.path.dirname(feature_store_file_path)
+            os.makedirs(dir_path,exist_ok=True)
+            dataframe.to_csv(feature_store_file_path,index=False,header=True)
+            return dataframe
+            
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+        
+    def split_data_as_train_test(self,dataframe: pd.DataFrame):
+        try:
+            train_set, test_set = train_test_split(
+                dataframe, test_size=self.data_ingestion_config.train_test_split_ratio
+            )
+            logging.info("Performed train test split on the dataframe")
+
+            logging.info(
+                "Exited split_data_as_train_test method of Data_Ingestion class"
+            )
+            
+            dir_path = os.path.dirname(self.data_ingestion_config.training_file_path)
+            
+            os.makedirs(dir_path, exist_ok=True)
+            
+            logging.info(f"Exporting train and test file path.")
+            
+            train_set.to_csv(
+                self.data_ingestion_config.training_file_path, index=False, header=True
+            )
+
+            test_set.to_csv(
+                self.data_ingestion_config.testing_file_path, index=False, header=True
+            )
+            logging.info(f"Exported train and test file path.")
+
+            
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
